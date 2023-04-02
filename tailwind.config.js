a185054dac10ea6f9f3781ceb80620ed3e20e8f9
Nick Gonzalez <nick.gonse@gmail.com>
2023-04-01 19:59:13 -0600

    update absent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type { import('tailwindcss').Config } */
 module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
@@ -7,7 +7,7 @@ module.exports = {
       colors: {
         'wordle-correct': '#5cb85c',
         'wordle-present': '#f0ad4e',
-        'wordle-absent': '#b0b0b0',
+        'wordle-absent': '#8a8a8a',
       },
     },
   },
